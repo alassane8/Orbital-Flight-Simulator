5973d310960c2d480f2db091aae732b68a020e71
alassane8 <alassane.wade@edu.ece.fr>
2026-05-18 23:27:45 +0200

    feat: created spaceflight dataclass + spaceflight status

---
+++ main/spaceflight/domain/spaceflight.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+
+@dataclass
+class Spaceflight: 
+    id: str
+    spaceflight_code: str
+    spaceflight_status: SpaceflightStatus
+    spaceship_id: str
+    departure_lauch_site_id: str
+    orbital_target_id: str
+    altitude_km: float
+    fuel_kg: float
+    fuel_burn_rate_kg_per_s: float
+
+    estimated_arrival_time: datetime = field(default_factory=datetime.now)
+    estimated_departure_time: datetime = field(default_factory=datetime.now)
+
+    arrival_time: datetime = field(default_factory=datetime.now)
+    departure_time: datetime = field(default_factory=datetime.now)
+
+    created_at: datetime = field(default_factory=datetime.now)
+    updated_at: datetime = field(default_factory=datetime.now)
---
+++ main/spaceflight/domain/spaceflight_status.py
@@ -0,0 +1,4 @@
+from enum import Enum
+
+class SpaceFlightStatus(str, Enum):
+
