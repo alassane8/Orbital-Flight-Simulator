76054bdd7ef77a53536437aeda378901382cc562
alassane8 <alassane.wade@edu.ece.fr>
2026-05-24 11:10:00 +0200

    fix: renamed spaceship with rocket

---
+++ main/rocket/domain/rocket.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass, field
+import datetime
+
+from main.orbital_target.domain.orbit_type import OrbitType
+
+@dataclass
+class Rocket:
+    id: str
+    name: str
+    manufacturer: str
+    nb_stages: int
+    max_payload_leo_kg: int 
+    max_payload_geo_kg: int
+    max_fuel_kg: int
+    fuel_burn_rate_kg_per_s: int
+    max_thrust_kn: int
+    max_speed_m_s: int
+    fairing_diameter_m: float
+    compatible_orbit_types: list[OrbitType]
+    description: str
+
+    created_at: datetime = field(default_factory=datetime.now)
+    updated_at: datetime = field(default_factory=datetime.now)
