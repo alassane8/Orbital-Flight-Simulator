a7fae40066d68f445f8687ecb551eca3a6bb9a79
alassane8 <alassane.wade@edu.ece.fr>
2026-05-29 12:59:52 +0200

    feat: updated lauch site status to match status in .json

--- main/launch_site/domain/launch_site_status.py
+++ main/launch_site/domain/launch_site_status.py
@@ -1,7 +1,9 @@
 from enum import Enum
 
+
 class LaunchSiteStatus(str, Enum):
+    ACTIVE = "ACTIVE"
+    INACTIVE = "INACTIVE"
     AVAILABLE = "AVAILABLE"
     MAINTENANCE = "MAINTENANCE"
     CLOSED = "CLOSED"
-
