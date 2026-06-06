05388141c2bb36b26e5b2c4934d2874ab37d945f
alassane8 <alassane.wade@edu.ece.fr>
2026-06-06 11:53:47 +0200

    feat: adding C++ files for orbital core functions

---
+++ main/orbital_core/orbital_core.cpp
@@ -0,0 +1,11 @@
+#include "orbital_core.h"
+
+double compute_delta_v(const LaunchSite& launch_site, const OrbitalTarget& target)
+{
+    // TODO
+}
+
+double compute_fuel(const Rocket& rocket, const std::vector<Payload>& payloads, double delta_v)
+{
+    // TODO
+}
---
+++ main/orbital_core/orbital_core.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vector>
+#include "models.h"
+
+/**
+ * Computes the delta-v (m/s) required to reach the orbital target from the given launch site.
+ * Takes into account the launch site latitude, the target orbit altitude, inclination,
+ * and the Earth's rotation contribution.
+ *
+ * @param launch_site  The launch site from which the rocket departs
+ * @param target       The orbital target (altitude, inclination, orbit type)
+ * @return             The required delta-v in m/s
+ */
+double compute_delta_v(const LaunchSite& launch_site, const OrbitalTarget& target);
+
+/**
+ * Computes the total fuel mass (kg) required for the mission using the Tsiolkovsky rocket equation.
+ * Accounts for the rocket's specific impulse, its dry mass, and the total payload mass.
+ *
+ * @param rocket    The rocket performing the mission
+ * @param payloads  The list of payloads on board
+ * @param delta_v   The required delta-v in m/s, as returned by compute_delta_v
+ * @return          The required fuel mass in kg
+ */
+double compute_fuel(const Rocket& rocket, const std::vector<Payload>& payloads, double delta_v);
