ec3f89440f0fbc9d76a101d05b346b49271f515f
alassane8 <alassane.wade@edu.ece.fr>
2026-06-07 10:14:28 +0200

    fix: function must return value in order to have a successful build

--- main/orbital_core/orbital_core.cpp
+++ main/orbital_core/orbital_core.cpp
@@ -2,10 +2,10 @@
 
 double compute_delta_v(const LaunchSite& launch_site, const OrbitalTarget& target)
 {
-    // TODO
+    return 0.0; // TODO: implémenter
 }
 
 double compute_fuel(const Rocket& rocket, const std::vector<Payload>& payloads, double delta_v)
 {
-    // TODO
+    return 0.0; // TODO: implémenter
 }
