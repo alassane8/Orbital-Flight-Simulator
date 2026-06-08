881a1c1a17c8bba7e56676a7d358e6761c30beca
alassane8 <alassane.wade@edu.ece.fr>
2026-06-08 13:15:02 +0200

    feat: constants file for maths operations

---
+++ main/orbital_core/constants.h
@@ -0,0 +1,5 @@
+namespace Constants {
+    constexpr double MU         = 3.986004418e14;  // m³/s²  — paramètre gravitationnel standard (WGS84)
+    constexpr double R_EARTH    = 6'378'137.0;     // m      — rayon équatorial terrestre (WGS84)
+    constexpr double OMEGA_EARTH = 7.2921150e-5;   // rad/s  — vitesse angulaire de rotation terrestre (IERS)
+}
