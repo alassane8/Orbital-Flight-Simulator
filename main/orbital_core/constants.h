3c3f9117dfbd5051ee167a6a3dff76de64a40c8c
alassane8 <alassane.wade@edu.ece.fr>
2026-06-09 12:56:12 +0200

    chore: new constants

--- main/orbital_core/constants.h
+++ main/orbital_core/constants.h
@@ -1,5 +1,7 @@
 namespace Constants {
-    constexpr double MU         = 3.986004418e14;  // m³/s²  — paramètre gravitationnel standard (WGS84)
-    constexpr double R_EARTH    = 6'378'137.0;     // m      — rayon équatorial terrestre (WGS84)
-    constexpr double OMEGA_EARTH = 7.2921150e-5;   // rad/s  — vitesse angulaire de rotation terrestre (IERS)
+    constexpr double MU          = 3.986004418e14;  // m³/s²  — paramètre gravitationnel standard (WGS84)
+    constexpr double R_EARTH     = 6'378'137.0;     // m      — rayon équatorial terrestre (WGS84)
+    constexpr double OMEGA_EARTH = 7.2921150e-5;    // rad/s  — vitesse angulaire de rotation terrestre (IERS)
+    constexpr double RHO_0       = 1.225;           // kg/m³ — densité air au niveau de la mer (ISA)
+    constexpr double H_SCALE     = 8500.0;          // m     — hauteur d'échelle atmosphérique
 }
